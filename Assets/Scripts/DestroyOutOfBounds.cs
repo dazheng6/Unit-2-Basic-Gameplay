1c7b26545659980820c7d61ec2ce961732c0a4ec
dazheng6 <121971803+dazheng6@users.noreply.github.com>
2023-01-30 14:12:35 -0800

    Fixed some code

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -18,7 +18,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound) 
         {
             Destroy(gameObject);
-        }   else if (transform.position.z > lowerBound) 
+        }   else if (transform.position.z < lowerBound) 
         {
             Destroy(gameObject);
         }
